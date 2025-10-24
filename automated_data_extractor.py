e351647695e7dc21e74a8b3794c92eb6337c1344
Emeigh <tre8@psu.edu>
2025-10-24 16:05:04 -0400

    Added checking for item stability. Making sure all targets loaded in dropdown.

--- automated_data_extractor.py
+++ automated_data_extractor.py
@@ -1,122 +1,126 @@
 import argparse
 from pathlib import Path
 import re
-import time
 
 from playwright.sync_api import sync_playwright, Playwright, expect
 
 TEST_ID_ATTRIBUTE = "data-test"
 DATA_EXTRACTOR_ADDRESS = "http://localhost:2900/tools/dataextractor"
-EXTRA_DELAY = 0.1
-CLICK_DELAY = 10
+EXTRA_DELAY = 100
 EMPTY_TABLE_REGEX = re.compile(r"No data available")
 
 
 def run_scraper(playwright: Playwright, download_directory: str) -> None:
     chromium = playwright.chromium
-    browser = chromium.launch(headless=False)
+    browser = chromium.launch(downloads_path=Path(download_directory), headless=False)
     data_extractor = browser.new_page()
     data_extractor.goto(DATA_EXTRACTOR_ADDRESS)
-    # Need to fix download to not overwrite
-    data_extractor.on(
-        "download",
-        lambda download: download.save_as(
-            Path(download_directory) / download.suggested_filename
-        ),
-    )
 
-    # Replace hard-coded password with .env
+    # ZZZ - Replace hard-coded password with .env
     data_extractor.get_by_role("textbox", name="Password").fill("Password")
-    data_extractor.get_by_role("button", name="Login").click(delay=CLICK_DELAY)
+    data_extractor.get_by_role("button", name="Login").click()
 
     telemetry_radio = data_extractor.get_by_role("radio", name="Telemetry")
     expect(telemetry_radio).to_be_visible()
-    time.sleep(EXTRA_DELAY)
-    telemetry_radio.click(delay=CLICK_DELAY, force=True)
+    telemetry_radio.scroll_into_view_if_needed()  # Forces stability check
+    telemetry_radio.click(force=True)
 
-    data_extractor.get_by_role("button", name="File").click(delay=CLICK_DELAY)
+    data_extractor.get_by_role("button", name="File").click()
     tab_delim_radio = data_extractor.get_by_role("radio", name="Tab Delimited")
     expect(tab_delim_radio).to_be_visible()
-    time.sleep(EXTRA_DELAY)
-    tab_delim_radio.click(delay=CLICK_DELAY, force=True)
+    tab_delim_radio.scroll_into_view_if_needed()  # Forces stability check
+    tab_delim_radio.click(force=True)
 
     mode_dropdown = data_extractor.get_by_role("button", name="Mode")
-    mode_dropdown.click(delay=CLICK_DELAY)
+    mode_dropdown.click()
     fill_down_checkbox = data_extractor.get_by_role("checkbox", name="Fill Down")
     expect(fill_down_checkbox).to_be_visible()
-    time.sleep(EXTRA_DELAY)
+    fill_down_checkbox.scroll_into_view_if_needed()
     if fill_down_checkbox.is_checked():
-        fill_down_checkbox.click(delay=CLICK_DELAY, force=True)
-        mode_dropdown.click(delay=CLICK_DELAY)
+        fill_down_checkbox.click(force=True)
+        mode_dropdown.click()
 
     matlab_header_checkbox = data_extractor.get_by_role(
         "checkbox", name="Matlab Header"
     )
     expect(matlab_header_checkbox).to_be_visible()
-    time.sleep(EXTRA_DELAY)
+    matlab_header_checkbox.scroll_into_view_if_needed()  # Forces stability check
     if matlab_header_checkbox.is_checked():
-        matlab_header_checkbox.click(delay=CLICK_DELAY, force=True)
-        mode_dropdown.click(delay=CLICK_DELAY)
+        matlab_header_checkbox.click(force=True)
+        mode_dropdown.click()
 
     unique_only_checkbox = data_extractor.get_by_role("checkbox", name="Unique Only")
     expect(unique_only_checkbox).to_be_visible()
-    time.sleep(EXTRA_DELAY)
+    unique_only_checkbox.scroll_into_view_if_needed()  # Forces stability check
     if unique_only_checkbox.is_checked():
-        unique_only_checkbox.click(delay=CLICK_DELAY, force=True)
-        mode_dropdown.click(delay=CLICK_DELAY)
+        unique_only_checkbox.click(force=True)
+        mode_dropdown.click()
 
     normal_columns_radio = data_extractor.get_by_role("radio", name="Normal Columns")
     expect(normal_columns_radio).to_be_visible()
-    time.sleep(EXTRA_DELAY)
-    normal_columns_radio.click(delay=CLICK_DELAY, force=True)
+    normal_columns_radio.scroll_into_view_if_needed()  # Forces stability check
+    normal_columns_radio.click(force=True)
 
     delete_all = data_extractor.get_by_test_id("delete-all")
     target_box = data_extractor.get_by_role("combobox").first
     packet_box = data_extractor.get_by_role("combobox").nth(1)
 
-    target_box.click(delay=CLICK_DELAY)
+    target_box.click()
     first_target = data_extractor.get_by_role("option").first
     expect(first_target).to_be_visible()
-    time.sleep(EXTRA_DELAY)
+    first_target.scroll_into_view_if_needed()  # Forces stability check
     targets = data_extractor.get_by_role("option").all()
-    for target in targets:
-        target.click(delay=CLICK_DELAY, force=True)
 
-        packet_box.click(delay=CLICK_DELAY)
+    # Need to load in all targets, not just ones that start active
+    prev_len = 0
+    curr_len = len(targets)
+    while curr_len != prev_len:
+        data_extractor.keyboard.press("ArrowUp")  # Goes to last loaded element
+        first_target.scroll_into_view_if_needed()  # Forces stability check
+        targets = data_extractor.get_by_role("option").all()
+        # Open and close box to get back to start
+        target_box.click()
+        target_box.click()
+        expect(first_target).to_be_visible()
+        first_target.scroll_into_view_if_needed()  # Forces stability check
+        prev_len = curr_len
+        curr_len = len(targets)
+
+    for target in targets:
+        target.click(force=True)
+
+        packet_box.click()
         first_packet = data_extractor.get_by_role("option").first
         expect(first_packet).to_be_visible()
-        time.sleep(EXTRA_DELAY)
+        first_packet.scroll_into_view_if_needed()  # Forces stability check
         packets = data_extractor.get_by_role("option").all()
         for packet in packets[1:]:
-            packet_box.click(delay=CLICK_DELAY)
-            first_packet = data_extractor.get_by_role("option").first
-            expect(first_packet).to_be_visible()
-            time.sleep(EXTRA_DELAY)
-            packet.click(delay=CLICK_DELAY, force=True)
+            packet.click(force=True)
 
-            data_extractor.get_by_role("button", name="Add Packet").click(
-                delay=CLICK_DELAY
-            )
+            data_extractor.get_by_role("button", name="Add Packet").click()
 
             packet_table = data_extractor.get_by_role("table")
             expect(packet_table).not_to_have_text(EMPTY_TABLE_REGEX)
-            time.sleep(EXTRA_DELAY)
 
-            data_extractor.get_by_role("button", name="Process").click(
-                delay=CLICK_DELAY
-            )
+            data_extractor.get_by_role("button", name="Process").click()
 
-            time.sleep(EXTRA_DELAY)
+            data_extractor.wait_for_timeout(EXTRA_DELAY)  # Avoid racing page
             process_progress = data_extractor.get_by_role("progressbar")
             expect(process_progress).to_have_attribute("aria-valuenow", "100")
-            delete_all.click(delay=CLICK_DELAY)
+            delete_all.click()
             expect(packet_table).to_have_text(EMPTY_TABLE_REGEX)
-            time.sleep(EXTRA_DELAY)
 
-        target_box.click(delay=CLICK_DELAY)
+            packet_box.click()
+            first_packet = data_extractor.get_by_role("option").first
+            expect(first_packet).to_be_visible()
+            first_packet.scroll_into_view_if_needed()  # Forces stability check
+
+        target_box.click()
         first_target = data_extractor.get_by_role("option").first
         expect(first_target).to_be_visible()
-        time.sleep(EXTRA_DELAY)
+        first_target.scroll_into_view_if_needed()  # Forces stability check
+
+    # ZZZ - HANDLE RENAMING DOWNLOADS
 
     browser.close()
 
